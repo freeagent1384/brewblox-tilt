43e7ed01977245e756cacb87460f17d4c436a74b
Bob Steers <steers.bob@gmail.com>
2021-05-31 19:01:29 +0200

    fix quotes in install script

--- install_tilt.py
+++ install_tilt.py
@@ -14,7 +14,7 @@ Steps:
 - Append the tilt service to ./docker-compose.yml.
 
 Notes:
-- Python >= 3.5 is required to run.
+- Python >= 3.6 is required to run.
 - This scripts depends on packages also used by brewblox-ctl
     - pyyaml
     - click
@@ -36,35 +36,35 @@ def install():
     Creates an empty configuration dir with the same name as the service.
     If you want to calibrate your tilt, place the calibration files in this directory.
     """
-    tilt_dir = path.abspath("./tilt")
-    compose_file = path.abspath("./docker-compose.yml")
+    tilt_dir = path.abspath('./tilt')
+    compose_file = path.abspath('./docker-compose.yml')
 
     if not path.exists(compose_file):
-        raise SystemExit("ERROR: Compose file not found in current directory. "
-                         "Please navigate to your brewblox directory first.")
+        raise SystemExit('ERROR: Compose file not found in current directory. '
+                         'Please navigate to your brewblox directory first.')
 
-    print("Creating ./tilt directory...")
+    print('Creating ./tilt directory...')
     if not path.exists(tilt_dir):
         makedirs(tilt_dir)
 
-    print("Editing docker-compose.yml file...")
+    print('Editing docker-compose.yml file...')
     with open(compose_file) as f:
         config = yaml.safe_load(f)
 
-    config["services"]["tilt"] = {
-        "image": "brewblox/brewblox-tilt:${BREWBLOX_RELEASE}",
-        "restart": "unless-stopped",
-        "privileged": True,
-        "network_mode": "host",
-        "volumes": ["./tilt:/share"]
+    config['services']['tilt'] = {
+        'image': 'brewblox/brewblox-tilt:${BREWBLOX_RELEASE}',
+        'restart': 'unless-stopped',
+        'privileged': True,
+        'network_mode': 'host',
+        'volumes': ['./tilt:/share']
     }
 
-    with open(compose_file, "w") as f:
+    with open(compose_file, 'w') as f:
         yaml.safe_dump(config, f)
 
-    print("Starting services...")
-    check_call("brewblox-ctl up", shell=True)
+    print('Starting services...')
+    check_call('brewblox-ctl up', shell=True)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     install()
