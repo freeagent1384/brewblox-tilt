947280637bcc4b8a5158bc7c78127793cf61f082
Bob Steers <steers.bob@gmail.com>
2021-11-10 16:29:49 +0100

    improve randomness in sim

--- brewblox_tilt/broadcaster_sim.py
+++ brewblox_tilt/broadcaster_sim.py
@@ -1,6 +1,6 @@
 import asyncio
 import time
-from random import random
+from random import uniform
 
 from aiohttp import web
 from brewblox_service import brewblox_logger, features, mqtt, repeater
@@ -10,10 +10,6 @@ LOGGER = brewblox_logger(__name__)
 
 def time_ms():
     return time.time_ns() // 1000000
-
-
-def neutral_random():
-    return random() - 0.5
 
 
 class BroadcasterSim(repeater.RepeaterFeature):
@@ -42,11 +38,11 @@ class BroadcasterSim(repeater.RepeaterFeature):
         await asyncio.sleep(self.interval)
         self.interval = self.active_scan_interval
 
-        self.temp_c += (neutral_random() * 10)
+        self.temp_c += uniform(-1, 1)
         self.temp_f = (self.temp_c * 9 / 5) + 32
-        self.sg += (neutral_random() / 100)
-        self.signal += neutral_random()
-        self.plato += (neutral_random() / 10)
+        self.sg += uniform(-0.01, 0.01)
+        self.signal += uniform(-1, 1)
+        self.plato += uniform(-0.1, 0.1)
 
         data = {
             'Temperature[degC]': self.temp_c,
